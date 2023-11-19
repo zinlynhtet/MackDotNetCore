bc569e4b12cda6591802a683009b4f6ec4a0b6a7
MacFlick <mackk5504@gmail.com>
2023-11-19 20:13:51 +0630

    Add project files.

---
+++ MackDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MackDotNetCore.ConsoleApp.EFCoreExamples;
+
+Console.WriteLine("Hello, World!");
+EFCoreExample eFCoreExample = new EFCoreExample();
+eFCoreExample.Run();
