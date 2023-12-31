08233dd619be90cf6a061b34274d8837416da5fb
MacFlick <mackk5504@gmail.com>
2023-12-31 20:42:33 +0630

    Modified ATM  MvcApp

--- MackDotNetCore.AtmApp/Controllers/BlogController.cs
+++ MackDotNetCore.AtmApp/Controllers/BlogController.cs
@@ -5,82 +5,69 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MackDotNetCore.AtmApp.Controllers
 {
-	public class BlogController : Controller
-	{
-		private readonly AppDbContext _context;
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
 
-		public BlogController(AppDbContext context)
-		{
-			_context = context;
-		}
-		//get
-		[ActionName("Index")]
-		public IActionResult BlogIndex()
-		{
-			List<BlogDataModel> lst = _context.Blogs.ToList();
-			return View("BlogIndex", lst);
-		}
-		[ActionName("Create")]
-		public IActionResult BlogCreate()
-		{
-			return View("BlogCreate");
-		}
-		[HttpPost]
-		[ActionName("Save")]
-		public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
-		{
-			await _context.Blogs.AddAsync(reqModel);
-			var result = await _context.SaveChangesAsync();
-			var message = result > 0 ? "Registration Successful." : "Registration failed.";
-			TempData["Message"] = message;
-			TempData["IsSuccess"] = result > 0;
-
-			return Redirect("/blog");
-		}
-		[ActionName("Withdraw")]
-		public IActionResult BlogWithdraw()
-		{
-			return View("BlogWithdraw");
-		}
-        private IActionResult WithdrawalFailed(string errorMessage)
+        public BlogController(AppDbContext context)
         {
-            TempData["Message"] = errorMessage;
-            TempData["IsSuccess"] = false;
+            _context = context;
+        }
+        //get
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            List<BlogDataModel> lst = _context.Blogs.ToList();
+            return View("BlogIndex", lst);
+        }
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
+        {
+            await _context.Blogs.AddAsync(reqModel);
+            var result = await _context.SaveChangesAsync();
+            var message = result > 0 ? "Registration Successful." : "Registration failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+
+            return Redirect("/blog");
+        }
+        [ActionName("Withdraw")]
+        public IActionResult BlogWithdraw()
+        {
+            return View("BlogWithdraw");
+        }
+    
+        [HttpPost]
+        [ActionName("Withdraw")]
+        public async Task<IActionResult> BlogWithdraw(BlogDataModel reqModel)
+        {
+            var blog = await _context.Blogs
+                .FirstOrDefaultAsync(b => b.CardNum == reqModel.CardNum && b.Pin == reqModel.Pin);
+
+            if (blog == null || reqModel.Balance <= 0 || reqModel.Balance > blog.Balance)
+            {
+                TempData["Message"] = "Withdrawal failed. Invalid data or insufficient balance.";
+                TempData["IsSuccess"] = false;
+
+                return View("BlogWithdraw");
+            }
+
+            blog.Balance -= reqModel.Balance;
+            _context.Blogs.Update(blog);
+
+            var result = await _context.SaveChangesAsync();
+
+            var message = result > 0 ? $"Withdraw Successful. Remaining Balance: {blog.Balance} $" : "Withdraw failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
             return Redirect("/home");
         }
-
-        [HttpPost]
-		[ActionName("Withdraw")]
-		public async Task<IActionResult> BlogWithdraw(BlogDataModel reqModel)
-		{
-			var blog = await _context.Blogs
-				.FirstOrDefaultAsync(b => b.CardNum == reqModel.CardNum && b.Pin == reqModel.Pin);
-
-			if (blog == null)
-			{
-				return WithdrawalFailed("Invalid cardNum or pin.");
-			}
-
-			if (reqModel.Balance <= 0)
-			{
-				return WithdrawalFailed("Invalid withdrawal amount.");
-			}
-
-			if (reqModel.Balance > blog.Balance)
-			{
-				return WithdrawalFailed("Insufficient balance.");
-			}
-
-			blog.Balance -= reqModel.Balance;
-			_context.Blogs.Update(blog);
-
-			var result = await _context.SaveChangesAsync();
-
-			var message = result > 0 ? "Withdrawal Successful." : "Withdrawal failed.";
-			TempData["Message"] = message;
-			TempData["IsSuccess"] = result > 0;
-			return Redirect("/home");
-		}
         [ActionName("Deposit")]
         public IActionResult BlogDeposit()
         {
@@ -93,18 +80,20 @@ namespace MackDotNetCore.AtmApp.Controllers
             var blog = await _context.Blogs
                 .FirstOrDefaultAsync(b => b.CardNum == reqModel.CardNum && b.Pin == reqModel.Pin);
 
+
             if (blog == null)
             {
-                return WithdrawalFailed("Invalid cardNum or pin.");
+                TempData["Message"] = "Invalid cardNum or pin.";
+                TempData["IsSuccess"] = false;
+                return View("BlogDeposit");
             }
-
 
             blog.Balance += reqModel.Balance;
             _context.Blogs.Update(blog);
 
             var result = await _context.SaveChangesAsync();
 
-            var message = result > 0 ? "Deposit Successful." : "Deposit failed.";
+            var message = result > 0 ? $"Deposit Successful. New Balance: {blog.Balance} $" : "Deposit failed.";
             TempData["Message"] = message;
             TempData["IsSuccess"] = result > 0;
             return Redirect("/home");
@@ -114,24 +103,17 @@ namespace MackDotNetCore.AtmApp.Controllers
         {
             return View("BlogCheck");
         }
-		[HttpGet]
-		[ActionName("Check")]
-		public async Task<IActionResult> BlogCheck(BlogDataModel reqModel)
-		{
-			var blog = await _context.Blogs
-				.FirstOrDefaultAsync(b => b.CardNum == reqModel.CardNum && b.Pin == reqModel.Pin);
-
+        [HttpGet]
+        [ActionName("Check")]
+        public async Task<IActionResult> BlogCheck(BlogDataModel reqModel)
+        {
+            var blog = await _context.Blogs
+                .FirstOrDefaultAsync(b => b.CardNum == reqModel.CardNum && b.Pin == reqModel.Pin);
             if (blog != null)
             {
                 return View("BlogCheckResult", blog);
             }
-            else
-            {
-				return View("BlogCheck");
-            }
-
-           
-           
-		}
-	}
+            return View("BlogCheck");
+        }
+    }
 }
