2a3097a173b6dd3af3786d135c06debb4d44e4ce
OrdinaryMack <mackk5504@gmail.com>
2024-01-10 23:24:50 +0630

    -

---
+++ MackDotNetCore.ConsoleApp/RefitExamples/IBlogApi.cs
@@ -0,0 +1,16 @@
+﻿using MackDotNetCore.ConsoleApp.Model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MackDotNetCore.ConsoleApp.RefitExamples
+{
+	public interface IBlogApi
+	{
+		[Get("/api/blog")]
+		Task<BlogListResponseModel> GetBlogLists();
+	}
+}
---
+++ MackDotNetCore.ConsoleApp/RefitExamples/RefitExample.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MackDotNetCore.ConsoleApp.RefitExamples
+{
+	public class RefitExample
+	{
+		private readonly IBlogApi blogApi = RestService.For<IBlogApi>("http://localhost:5095");
+		public async Task Run()
+		{
+			await Read();
+		}
+
+		private async Task Read()
+		{
+			var model = await blogApi.GetBlogLists();
+			foreach (var item in model.Data)
+			{
+				Console.WriteLine(item.blog_id);
+				Console.WriteLine(item.blog_title);
+				Console.WriteLine(item.blog_authour);
+				Console.WriteLine(item.blog_content);
+			}
+		}
+	}
+}
