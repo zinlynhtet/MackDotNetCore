a1903d2fc075f97bbd41641177fe4d8904337694
OrdinaryMack <mackk5504@gmail.com>
2024-01-11 12:10:38 +0630

    -

--- MackDotNetCore.ConsoleApp/RefitExamples/RefitExample.cs
+++ MackDotNetCore.ConsoleApp/RefitExamples/RefitExample.cs
@@ -27,42 +27,42 @@ namespace MackDotNetCore.ConsoleApp.RefitExamples
 				Console.WriteLine(item.blog_content);
 			}
 		}
-		private async Task Edit(int id)
-		{
-			var model = await blogApi.GetBlogList(id);
-			var item = model;
-				Console.WriteLine(item.blog_id);
-				Console.WriteLine(item.blog_title);
-				Console.WriteLine(item.blog_authour);
-				Console.WriteLine(item.blog_content);
-		}
-		private async Task Create(string title, string authour, string content)
-		{
-			BlogDataModel blog = new BlogDataModel
-			{
-				blog_title = title,
-				blog_authour = authour,
-				blog_content = content
-			};
-			var model = await blogApi.CreateBlogList(blog);
-            await Console.Out.WriteLineAsync(model.Message);
-        }
-		public async Task Update(int id, string title, string authour, string content)
-		{
-			BlogDataModel blog = new BlogDataModel
-			{
-				blog_title = title,
-				blog_authour = authour,
-				blog_content = content
-			};
-			var model = await blogApi.UpdateBlogList(blog);
-			await Console.Out.WriteLineAsync(model.Message);
-		}
-		public async Task Delete(int id)
-		{
-			var model = await blogApi.DeleteBlogList(id);
-			await Console.Out.WriteLineAsync(model.Message);
-		}
+		//private async Task Edit(int id)
+		//{
+		//	var model = await blogApi.GetBlogList(id);
+		//	var item = model;
+		//		Console.WriteLine(item.blog_id);
+		//		Console.WriteLine(item.blog_title);
+		//		Console.WriteLine(item.blog_authour);
+		//		Console.WriteLine(item.blog_content);
+		//}
+		//private async Task Create(string title, string authour, string content)
+		//{
+		//	BlogDataModel blog = new BlogDataModel
+		//	{
+		//		blog_title = title,
+		//		blog_authour = authour,
+		//		blog_content = content
+		//	};
+		//	var model = await blogApi.CreateBlogList(blog);
+  //          await Console.Out.WriteLineAsync(model.Message);
+  //      }
+		//public async Task Update(int id, string title, string authour, string content)
+		//{
+		//	BlogDataModel blog = new BlogDataModel
+		//	{
+		//		blog_title = title,
+		//		blog_authour = authour,
+		//		blog_content = content
+		//	};
+		//	var model = await blogApi.UpdateBlogList(blog);
+		//	await Console.Out.WriteLineAsync(model.Message);
+		//}
+		//public async Task Delete(int id)
+		//{
+		//	var model = await blogApi.DeleteBlogList(id);
+		//	await Console.Out.WriteLineAsync(model.Message);
+		//}
 	}
 	
 }
