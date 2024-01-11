603a902d43d5e22296b47880e37b82d447dcfcc2
OrdinaryMack <mackk5504@gmail.com>
2024-01-11 17:24:17 +0630

    -

---
+++ MackDotNetCore.MinimalApi/Model/BlogListResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MackDotNetCore.MinimalApi.Model
+{
+	public class BlogListResponseModel
+	{
+		public bool IsSuccess { get; set; }
+
+		public string Message { get; set; }
+
+		public List<BlogDataModel> Data { get; set; }
+	}
+}
---
+++ MackDotNetCore.MinimalApi/Model/BlogResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MackDotNetCore.MinimalApi.Model
+{
+	public class BlogResponseModel
+	{
+		public bool IsSuccess { get; set; }
+
+		public string Message { get; set; }
+
+		public BlogDataModel Data { get; set; }
+	}
+}
