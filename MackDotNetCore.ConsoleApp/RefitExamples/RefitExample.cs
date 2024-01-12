c84b8d67b9361d1ee3325512172954a9b5a43aa5
OrdinaryMack <mackk5504@gmail.com>
2024-01-12 19:43:55 +0630

    [Complete]Refit CRUD

--- MackDotNetCore.ConsoleApp/RefitExamples/RefitExample.cs
+++ MackDotNetCore.ConsoleApp/RefitExamples/RefitExample.cs
@@ -14,55 +14,76 @@ namespace MackDotNetCore.ConsoleApp.RefitExamples
 		public async Task Run()
 		{
 			await Read();
+			//await Edit(14);
+			//await Create("Hello", "this", "isRefit");
+			//await Update(17, "this", "is", "updateRefit");
+			//await Delete(50);
 		}
 
 		private async Task Read()
 		{
 			var model = await blogApi.GetBlogLists();
-			foreach (var item in model.Data)
+			if (model != null && model.Data != null)
 			{
-				Console.WriteLine(item.blog_id);
-				Console.WriteLine(item.blog_title);
-				Console.WriteLine(item.blog_authour);
-				Console.WriteLine(item.blog_content);
+				foreach (var item in model.Data)
+				{
+					Console.WriteLine(item.blog_id);
+					Console.WriteLine(item.blog_title);
+					Console.WriteLine(item.blog_authour);
+					Console.WriteLine(item.blog_content);
+				}
+			}
+			else
+			{
+				Console.WriteLine("No data returned from the server.");
 			}
 		}
-		//private async Task Edit(int id)
-		//{
-		//	var model = await blogApi.GetBlogList(id);
-		//	var item = model;
-		//		Console.WriteLine(item.blog_id);
-		//		Console.WriteLine(item.blog_title);
-		//		Console.WriteLine(item.blog_authour);
-		//		Console.WriteLine(item.blog_content);
-		//}
-		//private async Task Create(string title, string authour, string content)
-		//{
-		//	BlogDataModel blog = new BlogDataModel
-		//	{
-		//		blog_title = title,
-		//		blog_authour = authour,
-		//		blog_content = content
-		//	};
-		//	var model = await blogApi.CreateBlogList(blog);
-  //          await Console.Out.WriteLineAsync(model.Message);
-  //      }
-		//public async Task Update(int id, string title, string authour, string content)
-		//{
-		//	BlogDataModel blog = new BlogDataModel
-		//	{
-		//		blog_title = title,
-		//		blog_authour = authour,
-		//		blog_content = content
-		//	};
-		//	var model = await blogApi.UpdateBlogList(blog);
-		//	await Console.Out.WriteLineAsync(model.Message);
-		//}
-		//public async Task Delete(int id)
-		//{
-		//	var model = await blogApi.DeleteBlogList(id);
-		//	await Console.Out.WriteLineAsync(model.Message);
-		//}
+		private async Task Edit(int id)
+		{
+			var model = await blogApi.Getbloglist(id);
+			var item = model;
+			Console.WriteLine(item.blog_id);
+			Console.WriteLine(item.blog_title);
+			Console.WriteLine(item.blog_authour);
+			Console.WriteLine(item.blog_content);
+		}
+		private async Task Create(string title, string authour, string content)
+		{
+			BlogDataModel blog = new BlogDataModel
+			{
+				blog_title = title,
+				blog_authour = authour,
+				blog_content = content
+			};
+			var model = await blogApi.CreateBlogList(blog);
+			await Console.Out.WriteLineAsync(model.Message);
+		}
+		public async Task Update(int id, string title, string authour, string content)
+		{
+			BlogDataModel blog = new BlogDataModel
+			{
+				blog_id = id,
+				blog_title = title,
+				blog_authour = authour,
+				blog_content = content
+			};
+
+			var model = await blogApi.UpdateBlogList(id, blog);
+			await Console.Out.WriteLineAsync(model.Message);
+		}
+
+		private async Task Delete(int id)
+		{
+			var model = await blogApi.Getbloglist(id);
+			var result = await blogApi.DeleteBlog(id);
+			if (result != null)
+			{
+                await Console.Out.WriteLineAsync("Deleting successful.");
+            }
+			else
+			{
+				await Console.Out.WriteLineAsync("Deleting Failed.");
+			}
+		}
 	}
-	
 }
