aa3a69aba219104df0f805dcf7df58b8cf074863
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 14:52:22 +0630

    [Modified] ItemDataModel

---
+++ MackDotNetCore.ShoppingCardWebApp/Models/ItemDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MackDotNetCore.ShoppingCardWebApp.Models
+{
+    [Table("Tbl_item")]
+    public class ItemDataModel
+    {
+        [Key]
+        [Column("ItemId")]
+        public int? ItemId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
