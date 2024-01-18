8ce64fa64db2733e6a9cecba7ab1094d26deef3a
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 14:54:11 +0630

    -

--- MackDotNetCore.MinimalApi/Features/Blog/BlogService.cs
+++ MackDotNetCore.MinimalApi/Features/Blog/BlogService.cs
@@ -2,102 +2,112 @@
 using Microsoft.EntityFrameworkCore;
 using MackDotNetCore.MinimalApi.Model;
 using MackDotNetCore.MinimalApi;
+using System.Text.Json;
 
 public static class BlogService
 {
-	public static void AddBlogService(this IEndpointRouteBuilder app)
-	{
-		app.MapGet("/blog/{id}", async ([FromServices] AppDbContext db, int id) =>
-		{
-			var blog = await db.Blogs.AsNoTracking().FirstOrDefaultAsync(b => b.blog_id == id);
+    public static void AddBlogService(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/blog", async ([FromServices] AppDbContext db, ILogger<Program> _logger) =>
+        {
+            var lst = await db.Blogs.AsNoTracking().ToListAsync();
+            _logger.LogInformation("BlogLists =>" + JsonSerializer.Serialize(lst));
+            return (lst);
+        })
+        .WithName("GetBlog")
+        .WithOpenApi();
 
-			if (blog == null)
-			{
-				return Results.NotFound(new BlogResponseModel
-				{
-					IsSuccess = false,
-					Message = $"Blog with ID {id} not found"
-				});
-			}
+        app.MapGet("/blog/{id}", async ([FromServices] AppDbContext db, int id) =>
+        {
+            var blog = await db.Blogs.AsNoTracking().FirstOrDefaultAsync(b => b.blog_id == id);
 
-			return Results.Ok(new BlogResponseModel
-			{
-				IsSuccess = true,
-				Message = "Blog retrieved successfully",
-				Data = blog
-			});
-		})
-		.WithName("GetBlog")
-		.WithOpenApi();
+            if (blog == null)
+            {
+                return Results.NotFound(new BlogResponseModel
+                {
+                    IsSuccess = false,
+                    Message = $"Blog with ID {id} not found"
+                });
+            }
 
-		app.MapPost("/blog", async ([FromServices] AppDbContext db, BlogDataModel blog) =>
-		{
-			await db.Blogs.AddAsync(blog);
-			await db.SaveChangesAsync();
+            return Results.Ok(new BlogResponseModel
+            {
+                IsSuccess = true,
+                Message = "Blog retrieved successfully",
+                Data = blog
+            });
+        })
+        .WithName("GetByBlogId")
+        .WithOpenApi();
 
-			return Results.Created($"/blog/{blog.blog_id}", new BlogResponseModel
-			{
-				IsSuccess = true,
-				Message = "Blog created successfully",
-				Data = blog
-			});
-		})
-		.WithName("CreateBlog")
-		.WithOpenApi();
+        app.MapPost("/blog", async ([FromServices] AppDbContext db, BlogDataModel blog) =>
+        {
+            await db.Blogs.AddAsync(blog);
+            await db.SaveChangesAsync();
 
-		app.MapPut("/blog/{id}", async ([FromServices] AppDbContext db, int id, BlogDataModel updatedBlog) =>
-		{
-			var blog = await db.Blogs.AsNoTracking().FirstOrDefaultAsync(b => b.blog_id == id);
+            return Results.Created($"/blog/{blog.blog_id}", new BlogResponseModel
+            {
+                IsSuccess = true,
+                Message = "Blog created successfully",
+                Data = blog
+            });
+        })
+        .WithName("CreateBlog")
+        .WithOpenApi();
 
-			if (blog == null)
-			{
-				return Results.NotFound(new BlogResponseModel
-				{
-					IsSuccess = false,
-					Message = $"Blog with ID {id} not found"
-				});
-			}
+        app.MapPut("/blog/{id}", async ([FromServices] AppDbContext db, int id, BlogDataModel updatedBlog) =>
+        {
+            var blog = await db.Blogs.AsNoTracking().FirstOrDefaultAsync(b => b.blog_id == id);
 
-			blog.blog_title = updatedBlog.blog_title;
-			blog.blog_authour = updatedBlog.blog_authour;
-			blog.blog_content = updatedBlog.blog_content;
+            if (blog == null)
+            {
+                return Results.NotFound(new BlogResponseModel
+                {
+                    IsSuccess = false,
+                    Message = $"Blog with ID {id} not found"
+                });
+            }
 
-			await db.SaveChangesAsync();
+            blog.blog_title = updatedBlog.blog_title;
+            blog.blog_authour = updatedBlog.blog_authour;
+            blog.blog_content = updatedBlog.blog_content;
 
-			return Results.Ok(new BlogResponseModel
-			{
-				IsSuccess = true,
-				Message = "Blog updated successfully",
-				Data = blog
-			});
-		})
-		.WithName("UpdateBlog")
-		.WithOpenApi();
+            await db.SaveChangesAsync();
 
-		app.MapDelete("/blog/{id}", async ([FromServices] AppDbContext db, int id) =>
-		{
-			var blog = await db.Blogs.FirstOrDefaultAsync(b => b.blog_id == id);
+            return Results.Ok(new BlogResponseModel
+            {
+                IsSuccess = true,
+                Message = "Blog updated successfully",
+                Data = blog
+            });
+        })
+        .WithName("UpdateBlog")
+        .WithOpenApi();
 
-			if (blog == null)
-			{
-				return Results.NotFound(new BlogResponseModel
-				{
-					IsSuccess = false,
-					Message = $"Blog with ID {id} not found"
-				});
-			}
+        app.MapDelete("/blog/{id}", async ([FromServices] AppDbContext db, int id) =>
+        {
+            var blog = await db.Blogs.FirstOrDefaultAsync(b => b.blog_id == id);
 
-			db.Blogs.Remove(blog);
-			await db.SaveChangesAsync();
+            if (blog == null)
+            {
+                return Results.NotFound(new BlogResponseModel
+                {
+                    IsSuccess = false,
+                    Message = $"Blog with ID {id} not found"
+                });
+            }
 
-			return Results.Ok(new BlogResponseModel
-			{
-				IsSuccess = true,
-				Message = "Blog deleted successfully",
-				Data = blog
-			});
-		})
-		.WithName("DeleteBlog")
-		.WithOpenApi();
-	}
+            db.Blogs.Remove(blog);
+            await db.SaveChangesAsync();
+
+            return Results.Ok(new BlogResponseModel
+            {
+                IsSuccess = true,
+                Message = "Blog deleted successfully",
+                Data = blog
+            });
+        })
+        .WithName("DeleteBlog")
+        .WithOpenApi();
+    }
 }
