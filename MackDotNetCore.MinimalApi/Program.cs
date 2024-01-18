e4c2da702032ad602607738f8df209ee0dbd60e8
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 14:53:58 +0630

    [Add] Database Connection String

--- MackDotNetCore.MinimalApi/Program.cs
+++ MackDotNetCore.MinimalApi/Program.cs
@@ -1,49 +1,70 @@
 using MackDotNetCore.MinimalApi;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Hour, fileSizeLimitBytes: 1024)
+    .CreateLogger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.Configure<JsonOptions>(options =>
+try
 {
-	options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-	options.SerializerOptions.PropertyNamingPolicy = new UpperCaseNamingPolicy(); 
-});
+    Log.Information("Starting web application");
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-	string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
-	options.UseSqlServer(connectionString);
-},
-ServiceLifetime.Transient,
-ServiceLifetime.Transient);
+    builder.Host.UseSerilog();
 
-var app = builder.Build();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.Configure<JsonOptions>(options =>
+    {
+        options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.SerializerOptions.PropertyNamingPolicy = new UpperCaseNamingPolicy();
+    });
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
+        options.UseSqlServer(connectionString);
+    },
+    ServiceLifetime.Transient,
+    ServiceLifetime.Transient);
+
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.AddBlogService();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.AddBlogService();
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 public class UpperCaseNamingPolicy : JsonNamingPolicy
 {
-	public override string ConvertName(string name)
-	{
-		if (string.IsNullOrEmpty(name))
-			return name;
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return name;
 
-		char[] chars = name.ToCharArray();
-		chars[0] = char.ToUpper(chars[0]);
-		return new string(chars);
-	}
+        char[] chars = name.ToCharArray();
+        chars[0] = char.ToUpper(chars[0]);
+        return new string(chars);
+    }
+
 }
