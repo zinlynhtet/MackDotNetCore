f0b01c3d0620a889dcfe6a7db52d66a6e75b2fa3
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 14:52:45 +0630

    [Add] ItemDataResponseModel

---
+++ MackDotNetCore.ShoppingCardWebApp/Models/ItemDataResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MackDotNetCore.ShoppingCardWebApp.Models
+{
+    public class ItemDataResponseModel
+    {
+        public int? ItemId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public List<ItemDataModel> Data { get; set; }
+    }
+}
