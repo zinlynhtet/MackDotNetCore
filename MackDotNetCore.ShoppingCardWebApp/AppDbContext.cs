f24365c5757b279fd4ec0b979b80dcd0ed03a969
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 14:51:54 +0630

    [Add] AppDbContext

---
+++ MackDotNetCore.ShoppingCardWebApp/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MackDotNetCore.ShoppingCardWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MackDotNetCore.ShoppingCardWebApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ItemDataModel> Data { get; set; }
+    }
+}
