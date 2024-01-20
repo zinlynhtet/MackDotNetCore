e292559654346297815aafdce5586085ae49e1e6
OrdinaryMack <mackk5504@gmail.com>
2024-01-20 12:41:02 +0630

    [Complete} NLog

---
+++ MackDotNetCore.NlogExampleConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+
+internal class Program
+{
+    private static void Main()
+    {
+        var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
+        logger.LogInformation("Program has started.");
+        Console.ReadKey();
+    }
+}
