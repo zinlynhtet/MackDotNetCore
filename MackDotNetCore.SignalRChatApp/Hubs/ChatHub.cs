88e9cd3c2597fbb59dd93d29f5090beb93cd650a
OrdinaryMack <mackk5504@gmail.com>
2024-01-22 02:58:52 +0630

    [Add] ChatHub

---
+++ MackDotNetCore.SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MackDotNetCore.SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SeverReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
