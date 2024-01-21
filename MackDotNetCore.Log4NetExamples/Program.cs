43c0825f0cb171f3a265a10b36e04eb12fc3fdfc
OrdinaryMack <mackk5504@gmail.com>
2024-01-21 14:57:40 +0630

    -

---
+++ MackDotNetCore.Log4NetExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace MackDotNetCore.Log4NetExamples
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            // Configure log4net using the XML configuration file
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+            // Log some messages
+            log.Debug("This is a debug message");
+            log.Info("This is an info message");
+            log.Warn("This is a warning message");
+            log.Error("This is an error message");
+            log.Fatal("This is a fatal error message");
+
+            Console.WriteLine("Check the console for log messages. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
