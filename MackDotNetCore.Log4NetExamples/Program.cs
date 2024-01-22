09c333015441077481eb22f25da2769c7b6be6ed
OrdinaryMack <mackk5504@gmail.com>
2024-01-23 03:25:21 +0630

    -

--- MackDotNetCore.Log4NetExamples/Program.cs
+++ MackDotNetCore.Log4NetExamples/Program.cs
@@ -3,27 +3,20 @@ using log4net.Config;
 using System;
 using System.Reflection;
 
-namespace Log4netExample
-{
-    class Program
-    {
-        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
-        static void Main(string[] args)
-        {
-            // Get the name of the currently executing assembly
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string projectName = assembly.GetName().Name!;
+ILog log = LogManager.GetLogger(typeof(Program));
 
-            // Configure log4net using the XML configuration file
-            XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
 
-            // Log some messages with the project name
-            log.Info($"{projectName} - This is an info message");
-            log.Warn($"{projectName} - This is a warning message");
+Assembly assembly = Assembly.GetExecutingAssembly();
+string projectName = assembly.GetName().Name!;
 
-            Console.WriteLine("Check the console for log messages. Press any key to exit.");
-            Console.ReadKey();
-        }
-    }
-}
+XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+log.Info($"{projectName} - This is an info message");
+log.Warn($"{projectName} - This is a warning message");
+
+Console.WriteLine("Check the console for log messages. Press any key to exit.");
+Console.ReadKey();
+
+
+
