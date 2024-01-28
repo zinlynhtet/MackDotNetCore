f5f9438da8b4894598365fe087c294c6ca951532
ZinLinnhtet <mackk5504@gmail.com>
2024-01-28 22:35:29 +0630

    -

--- MackDotNetCore.DbFirstCommandApp/Models/TblBlog.cs
+++ MackDotNetCore.DbFirstCommandApp/Models/TblBlog.cs
@@ -6,10 +6,7 @@ namespace MackDotNetCore.DbFirstCommandApp.Models;
 public partial class TblBlog
 {
     public int BlogId { get; set; }
-
     public string BlogTitle { get; set; } = null!;
-
     public string BlogAuthor { get; set; } = null!;
-
     public string BlogContent { get; set; } = null!;
 }
